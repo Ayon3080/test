31a46d2b6b7c43cb934369bf20d1d51ada785861
Ayon Ghosh <ayon3080@gmail.com>
2026-05-03 02:41:51 +0600

    Commit

---
+++ Desktop/Final_Web_Tech_Practic1/View/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+</head>
+<body>
+    <table>
+        <tr>
+            <td>username</td>
+            <td><input type="text" name="username"/></td>
+
+        </tr>
+
+        
+    </table>
+
+    
+</body>
+</html>
